d34e7e67c4e6a18ce9fae29904a05905e13cf817
JB Hemmes <pitchblackcat@gmail.com>
2019-09-08 05:11:20 +0200

    tweaked the command a bit

--- src/main/java/nl/awesome/cli/NeuralRacer.java
+++ src/main/java/nl/awesome/cli/NeuralRacer.java
@@ -1,35 +1,46 @@
 package nl.awesome.cli;
 
-import nl.awesome.neural.factory.HiddenLayerNetworkFactory;
 import nl.awesome.neuralracer.NeuralRacerClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import picocli.CommandLine;
 
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "NeuralRacer", header = "%n@|green Neural Racer|@")
 public class NeuralRacer implements Callable<Integer> {
-    @CommandLine.Option(names = {"-p", "--port"}, required = false, description = "socket port", defaultValue = "8888")
-    String port;
+    public static final Logger logger = LogManager.getLogger(NeuralRacer.class);
 
-    @CommandLine.Option(names = {"-h", "--host"}, required = false, description = "socket host", defaultValue = "fe80::4ddc:287:3750:f172")
+    @CommandLine.Option(names = {"-p", "--port"}, description = "socket port", defaultValue = "8888", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    int port;
+
+    @CommandLine.Option(names = {"-h", "--host"}, description = "socket host", defaultValue = "fe80::4ddc:287:3750:f172", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
     String host;
 
-    @CommandLine.Option(names = {"-i", "--initial-players"}, required = false, description = "number of initial players", defaultValue = "100")
-    String initalPlayers;
+    @CommandLine.Option(names = {"-i", "--initial-players"}, description = "number of initial players", defaultValue = "50", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    int initalPlayers;
 
-    @CommandLine.Option(names = {"-t", "--top-players"}, required = false, description = "the number of top players will spawn children for the next generation", defaultValue = "10")
-    String topPlayers;
+    @CommandLine.Option(names = {"-t", "--top-players"}, description = "when a next generation starts, <topPlayers> will be selected to mutate.", defaultValue = "10", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    int topPlayers;
 
-    @CommandLine.Parameters(/* type = File.class, */ description = "hidden layers")
-    List<Integer> hiddenLayers; // picocli infers type from the generic type
+    @CommandLine.Parameters(description = "hidden layers")
+    List<Integer> hiddenLayers;
 
     public static void main(String... args) {
-        new CommandLine(new NeuralRacer()).execute(args);
+        CommandLine.Help.Ansi ansi = CommandLine.Help.Ansi.ON;
+        System.exit(
+                new CommandLine(new NeuralRacer())
+                        .setOut(new PrintWriter(System.out))
+                        .setErr(new PrintWriter(System.err))
+                        .setColorScheme(CommandLine.Help.defaultColorScheme(ansi))
+                        .execute(args)
+        );
     }
 
     public Integer call() {
-        NeuralRacerClient.startServer(host, Integer.parseInt(port), Integer.parseInt(initalPlayers), Integer.parseInt(topPlayers), hiddenLayers);
+        NeuralRacerClient.startServer(host, port, initalPlayers, topPlayers, hiddenLayers);
         return 0;
     }
 }
